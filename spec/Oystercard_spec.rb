ed0b8c337db0bc524abbb4594c22c8a3cedb8c0e
KiranKukar <kiranrkukar@gmail.com>
2021-12-14 14:26:03 +0000

    added default balance and test

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,5 +1,10 @@
 require 'Oystercard'
 
 describe Oystercard do
+
+  it "checks the cards balance is initialized at 0" do
+    card = Oystercard.new
+    expect(card.balance).to eq 0
+  end
   
 end
