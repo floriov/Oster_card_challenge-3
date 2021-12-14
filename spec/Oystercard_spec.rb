cdb910199d6b5e2f33fcbb59dab048155de5a496
mikitaro-27 <matt.diak@gmail.com>
2021-12-14 14:49:19 +0000

    create top_up method for Oystercard class

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -3,5 +3,16 @@ require 'Oystercard'
 describe Oystercard do
 
   it { is_expected.to have_attributes(balance: 0) }
-  
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  # we want to test whether top_up method adds amount to balance
+  describe '#top_up' do
+    it 'adds to balance when topping up' do
+      #arrange
+      subject.top_up(40)
+      subject.top_up(20)
+      #assert
+      expect(subject.balance).to eq 40
+    end
+  end
 end
