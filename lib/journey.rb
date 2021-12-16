79f7d60bb4b615cb3792a87fd875f0e6035ceeda
Vanessa Florio <vanessaflorio1997@gmail.com>
2021-12-16 17:24:43 +0000

    Added fare(card) method

--- lib/journey.rb
+++ lib/journey.rb
@@ -7,22 +7,21 @@ class Journey
     end
 
     def entry_station(card, station)
-        card.get_fare(PENALTY_FARE) unless @journey.empty?
+        fare(card) unless @journey.empty?
         card.journeys << @journey unless @journey.empty?
         @journey = {entry_station: station}
       end
     
       def exit_station(card, station)
-        @journey[:entry_station] ? card.get_fare(MINIMUM_FARE) : card.get_fare(PENALTY_FARE) 
-        # if @journey[:entry_station]
-        #   deduct(MINIMUM_FARE)
-        # else
-        #   deduct(PENALTY_FARE)
-        # end
-    
         @journey[:exit_station] = station
+        fare(card)
         card.journeys << @journey
         @journey = {}
+      end
+
+      def fare(card)
+        return card.get_fare(MINIMUM_FARE) if @journey[:entry_station] && @journey[:exit_station]
+        card.get_fare(PENALTY_FARE) 
       end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,4 +2,7 @@ require 'journey'
 
 describe '#journeys' do
  
+  it 'knows if a journey is complete' do 
+
+  end
 end
